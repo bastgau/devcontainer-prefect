0e4c1814e79e074a6b5d2dc596115044e6f4bbac
Bastien Gautier <bastien.gautier@dcube.fr>
2022-12-12 10:22:55 +0100

    :tada:  Init new prefect project

---
+++ .devcontainer/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo chgrp vscode /workspaces/app/.venv
+sudo chown vscode /workspaces/app/.venv
+
+git config --global --add safe.directory /workspaces/app
+git config core.eol lf
+git config core.autocrlf false
+
+python3 -m venv /workspaces/app/.venv
+PATH="/workspaces/app/.venv/bin:$PATH"
+
+source /workspaces/app/.venv/bin/activate
+pip install --upgrade pip
+
+# pip install keyring artifacts-keyring
+
+# cat <<EOF >> /workspaces/app/.venv/pip.conf
+# [global]
+# extra-index-url=https://pkgs.dev.azure.com/...
+# EOF
+
+pip install -r /workspaces/app/requirements-dev.txt
+pip install -r /workspaces/app/requirements.txt
